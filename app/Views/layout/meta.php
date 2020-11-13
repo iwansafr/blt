402284f86d0c0c6538d829750c031196f1616223
iwansafr <iwansafr@gmail.com>
2020-11-13 12:42:10 +0700

    templates change path dist

--- app/Views/layout/meta.php
+++ app/Views/layout/meta.php
@@ -4,7 +4,7 @@
 <meta name="description" content="" />
 <meta name="author" content="" />
 <title>Dashboard - </title>
-<link href="/template/dist/css/styles.css" rel="stylesheet" />
+<link href="/template/css/styles.css" rel="stylesheet" />
 <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
 <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
 <?= csrf_meta() ?>
